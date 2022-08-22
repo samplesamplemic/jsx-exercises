b0efc064ed6a56ce36f8fd41e955f16f2c448b06
Mic <michelelt660@gmail.com>
2022-08-22 13:23:44 +0200

    jsx-2 ex

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
 function App() {
-  const hello = <h1 className="text-3xl font-bold flex justify-center mt-4">Hello, World!</h1>;
- 
+  const Myf = (props) => {
+  return <h1 className="text-3xl font-bold flex justify-center mt-4">Hello, {props.name} </h1>
+ }
+ const myf2 = (name) => {
+  return <h1 className="text-3xl font-bold flex justify-center mt-4">Hello, {name} </h1>
+ }
+  
+ //which is the best practice?
+
   return (
     <>
-     {hello}
+     <Myf name="mic"/>
+     {myf2('mic')}
     </>
   );
 }
